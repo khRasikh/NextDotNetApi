5206169acf14a2a9183d5e7db9853e829f9af393
khRasikh <kh.rasikh542@gmail.com>
2025-12-04 18:26:37 +0100

    adding Swagger documentation

--- NextDotNetApi/Program.cs
+++ NextDotNetApi/Program.cs
@@ -12,7 +12,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
 
 app.UseHttpsRedirection();
 
